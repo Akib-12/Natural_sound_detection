9ee5ba07116bdf2fe16a37cd0ac898a62b8d6ab9
Akib-12 <37499896+Akib-12@users.noreply.github.com>
2021-04-05 16:34:59 +0600

    Used all the 2K sound clips and got the better result

--- get_MFCCs.py
+++ get_MFCCs.py
@@ -1,29 +1,41 @@
 #%%
-import librosa
+import json
+import math
+
 import librosa.display
 import pandas as pd
-import json
-
 
 #%%
-def get_mfcc(filepath):
-    signal, sr = librosa.load(filepath, sr=None)
-    return librosa.feature.mfcc(signal, sr=sr).tolist()
-
+SAMPLE_RATE = 22050
+TRACK_DURATION = 5 # measured in seconds
+SAMPLES_PER_TRACK = SAMPLE_RATE * TRACK_DURATION
+num_segments = 5
+samples_per_segment = SAMPLES_PER_TRACK / num_segments
+hop_length = 512
+num_mfcc_vectors_per_segment = math.ceil(samples_per_segment / hop_length)
 
 #%%
 extracted_feature_mfcc = []
 
 df = pd.read_csv('dataset_all.csv')
+
 for row in df.itertuples():
-    filepath = 'audio/' + row.filename
-    mfcc = get_mfcc(filepath)
-    extracted_feature_mfcc.append({
-        "filename": row.filename,
-        "category": row.category,
-        "target": row.target,
-        "mfcc": mfcc
-    })
+    file_path = 'audio/' + row.filename
+
+    signal, sr = librosa.load(file_path, sr=None)
+
+    for i in range(5):
+        seg = i + 1
+        start = int(samples_per_segment) * seg
+        finish = start + int(samples_per_segment)
+        extracted_feature_mfcc.append({
+            "filename": row.filename,
+            "category": row.category,
+            "target": row.target,
+            "mfcc": librosa.feature.mfcc(signal[start : finish], sr=sr / 2).tolist()
+        })
+
+
 
 #%%
 
